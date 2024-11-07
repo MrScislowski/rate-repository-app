13e9fa7a5550456f7b0b64951409ad908ddbcd96
Daniel Scislowski <mr.scislowski@gmail.com>
2024-11-07 10:48:38 -0600

    WIP

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,12 +1,37 @@
-import { Text, View } from "react-native";
+import { Text, View, Image, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  headerBar: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  titleText: {
+    fontWeight: "bold",
+  },
+  descriptionText: {},
+  languageText: {
+    backgroundColor: "#005eff",
+    color: "#ffffff",
+    marginLeft: 70,
+  },
+});
 
 const RepositoryItem = (props) => {
   const item = props.item;
+  console.log(item.ownerAvatarUrl);
   return (
     <View>
-      <Text>Full name: {item.fullName} </Text>
-      <Text>Description: {item.description} </Text>
-      <Text>Language: {item.language} </Text>
+      <View style={styles.headerBar}>
+        <Image
+          height={50}
+          width={50}
+          source={{ uri: item.ownerAvatarUrl }}
+        ></Image>
+        <Text style={styles.titleText}>{item.fullName} </Text>
+        <Text style={styles.descriptionText}>{item.description} </Text>
+      </View>
+      <Text style={styles.languageText}>{item.language} </Text>
       <Text>Stars: {item.stargazersCount} </Text>
       <Text>Forks: {item.forksCount} </Text>
       <Text>Reviews: {item.reviewCount} </Text>

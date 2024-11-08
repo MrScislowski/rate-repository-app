997e20de8b326f4906b23dda87e30bf7232ee8ed
Daniel Scislowski <mr.scislowski@gmail.com>
2024-11-08 10:33:01 -0600

    WIP

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,7 +1,33 @@
-import { Text } from "react-native";
+import { Alert, StyleSheet, Text, TextInput } from "react-native";
+import { useFormik } from "formik";
+import theme from "../theme";
+
+const styles = StyleSheet.create({
+  textinput: {
+    margin: 10,
+    padding: 10,
+    borderColor: theme.backdropColor,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+});
 
 const SignIn = () => {
-  return <Text>The sign-in view</Text>;
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
+    },
+    onSubmit: (values) => {
+      Alert.alert(JSON.stringify(values, null, 2));
+    },
+  });
+
+  return (
+    <>
+      <TextInput style={styles.textinput} placeholder="Username"></TextInput>
+    </>
+  );
 };
 
 export default SignIn;

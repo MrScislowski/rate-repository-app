787293be8a58ab558c567cdfba13ca53acbaefa2
Daniel Scislowski <mr.scislowski@gmail.com>
2024-11-11 13:42:36 -0600

    Exercise 10.12: environment variables

--- src/utils/apolloClient.js
+++ src/utils/apolloClient.js
@@ -1,12 +1,15 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import Constants from "expo-constants";
 
 const createApolloClient = () => {
+  console.log("expoconfig: ");
+  console.log(JSON.stringify(Constants.expoConfig, null, 2));
   return new ApolloClient({
     // POWERSHELL:
     // Get-NetIPConfiguration | Where-Object { $_.InterfaceAlias -eq "Wi-Fi"} | Select-Object -Property IPv4Address
     // MAC:
     // ifconfig | grep 192.160.0
-    uri: "http://192.168.0.133:4000/graphql",
+    uri: Constants.expoConfig.extra.APOLLO_URL,
     cache: new InMemoryCache(),
   });
 };
